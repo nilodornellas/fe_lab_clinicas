ba249465d14ab77f018bfa2c9cbbbc81fa08d89a
Nilo Dornellas <iannilo01@gmail.com>
2024-09-11 21:48:28 -0300

    Day1 and Day2 of flutter experience

---
+++ lib/src/pages/splash_page/splash_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Navigator.of(context).pushReplacementNamed('/auth/login');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(''),
+        ),
+        body: Center(
+          child: Image.asset('assets/images/logo_vertical.png'),
+        ));
+  }
+}
